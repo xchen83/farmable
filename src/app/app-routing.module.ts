d81a235716c06ef8b87b611da96bd6cd64d955c5
Xuan Chen <xchen83@uw.edu>
2025-01-30 23:01:04 -0800

    Update Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+    { path: '', component: DashboardComponent },
+    { path: '**', redirectTo: '' },
+    { path: 'add-produce', loadChildren: () => import('./add-produce/add-produce.module').then(m => m.AddProduceModule) },
+    { path: 'add-inventory', loadChildren: () => import('./add-inventory/add-inventory.module').then(m => m.AddInventoryModule) },
+    { path: 'product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: DashboardComponent },
+    { path: '', component: DashboardComponent },
     { path: 'add-produce', loadChildren: () => import('./add-produce/add-produce.module').then(m => m.AddProduceModule) },
     { path: 'add-inventory', loadChildren: () => import('./add-inventory/add-inventory.module').then(m => m.AddInventoryModule) },
     { path: 'product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) },
